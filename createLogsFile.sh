3a9a1dcbefc9f7552c33bfb43d472c36ccf8ef29
Joseph-Antoine Martineau-Gagné <joseph-antoine@spaceconcordia.ca>
2013-12-16 23:39:27 -0500

    bash script that creates 100 files containing 100 times '1234567890'

---
+++ createLogsFile.sh
@@ -0,0 +1,19 @@
+#
+#
+#
+#
+
+FOLDER="."
+STR="1234567890"
+FILE="ACS.1.log"
+
+if [ -n "$1" ]; then
+    FOLDER=$1
+fi
+
+for j in `seq 1 100`; do
+    for i in `seq 1 100`; do
+        echo $STR >> $FOLDER"/ACS."$j".log"
+    done
+done
+
