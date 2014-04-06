8c8d86f125727f1381a3f7767143178fd9217848
Joseph-Antoine Martineau-Gagné <joseph-antoine@spaceconcordia.ca>
2014-04-06 00:56:13 -0400

    Added global symbols header SpaceDecl.h

---
+++ include/SpaceDecl.h
@@ -0,0 +1,25 @@
+/*=============================================================================
+*
+*   FILE        : SpaceDecl.h
+*
+*   AUTHOR      : Space Concordia 2014 
+*
+*   PURPOSE     : This header contains Global Symbols.
+*
+*
+*============================================================================*/
+
+
+#ifndef SPACE_CONCORDIA_DECL_H
+#define SPACE_CONCORDIA_DECL_H
+
+
+
+#define CS1_APPS            "/home/apps"
+#define CS1_LOGS            "/home/logs"
+#define CS1_TGZ             "/home/tgz"
+#define CS1_PIPES           "/home/pipes"
+#define CS1_WATCH_PUPPY_PID "/home/pids/watch-puppy.pid"
+
+
+#endif
