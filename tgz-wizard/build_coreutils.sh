ceab3ba5d09efb0adc4ac47badbe452dc5aca9e7
Joseph-Antoine Martineau-Gagné <joseph-antoine@spaceconcordia.ca>
2014-05-19 01:57:21 -0400

    build coreutils

--- tgz-wizard/build_coreutils.sh
+++ tgz-wizard/build_coreutils.sh
@@ -2,6 +2,8 @@
 #**********************************************************************************************************************
 #
 # AUTHORS : Space Concordia 2014, Joseph
+# 
+# PURPOSE : download/compile coreutils for Q6 (in particular : we needed to compile the 'split' command)
 #
 # REFERENCE : http://git.savannah.gnu.org/cgit/coreutils.git/plain/README-hacking
 #
@@ -9,14 +11,18 @@
 
 COREUTILS_GIT="git://git.sv.gnu.org/coreutils"          #   git repo, assuming you have git installed
 apps=(autoconf automake autopoint gperf texinfo)        #   those are needed to build coreutils
-$COREUTILS_PATH="./coreutils"
+COREUTILS_PATH="./coreutils"
 
 read -p "clone $COREUTILS_GIT ? [y|N]" choice
 case "$choice" in
     y|Y)    git clone $COREUTILS_GIT;;
     n|N|'') echo "Skipping cloning of $COREUTILS_GIT..."; echo;;
 esac
+echo;
 
+#
+# Installs dependencies
+#
 for app in ${apps[@]}
 do
 	read -p "Install $app ? [y|N]" choice
@@ -24,13 +30,37 @@ do
 	  y|Y ) apt-get install $app;;
 	  n|N|'' ) echo "Skipping installation of $app..."; echo;;
 	esac
+
+    echo;
 done
 
-read -p "run the bootstraping now? ? [y|N]" choice
+#
+# boostrap
+#
+read -p "run the bootstraping now? [y|N]" choice
 case "$choice" in
-    y|Y)    cd $COREUTILS_PATH; ./bootstrap;
-            echo "Now you can run : ./configure && make && make check" echo;;   ### remove Werror from the generated makefile!!!
+    y|Y)    cd $COREUTILS_PATH; ./bootstrap;;
     n|N|'') echo "Skipping bootstrap step..."; echo;;
 esac
+echo;
 
+#
+# ./configure
+#
+read -p "Configure now? (should NOT be root for this step!) [y|N]" choice
+case "$choice" in
+    y|Y)    sh cross_compile_coreutils.sh ;;
+    n|N|'') echo "Skipping configuration step..."; echo;;
+esac
+
+#
+# farewell
+#
+echo;
+echo "Now edit the Makefile : remove the 'Werror' flag!"
+echo "Run 'make', the binaries are under ./coreutils/src"
+
+#
+# exit
+# 
 echo "exit"
