f975041af8f688e6550419273e85a0d1dc762c72
harley <harley.1011@gmail.com>
2015-05-28 03:08:34 +0000

    Fixed issues with settime script

--- testing/settime_rtc_commander_test.sh
+++ testing/settime_rtc_commander_test.sh
@@ -1,12 +1,12 @@
 dir="/home/pipes/"
-echo -n -e \x01 > $dir/Inet-w-com-r
+echo -n -e "\x06" > $dir/Inet-w-com-r
 
-echo -n -e \x30\xD8\x56\xB1\x18 > $dir/Dnet-w-com-r
+echo -n -e "\x30\x64\x00\x00\x00\x00" > $dir/Dnet-w-com-r
 
-echo -n -e \xFF > $dir/Inet-w-com-r
+echo -n -e "\xFF" > $dir/Inet-w-com-r
 
-echo -n -e \x01 > $dir/Inet-w-com-r
+echo -n -e "\x01" > $dir/Inet-w-com-r
 
-echo -n -e \x21 > $dir/Dnet-w-com-r
+echo -n -e "\x21" > $dir/Dnet-w-com-r
 
-echo -n -e \xFF > $dir/Inet-w-com-r
+echo -n -e "\xFF" > $dir/Inet-w-com-r
